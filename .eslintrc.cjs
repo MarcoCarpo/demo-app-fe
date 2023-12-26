150d9a1ea9db55a40a3952b16bd1e43d56ddccb7
Marco Carpona <marco.carpona@gmail.com>
2023-12-26 09:26:07 +0100

    Initialize project and install tailwindcss

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+import eslintConfigPrettier from "eslint-config-prettier";
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "airbnb",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "prettier"],
+  rules: {
+    "no-tabs": ["error", { allowIndentationTabs: true }],
+  },
+};
